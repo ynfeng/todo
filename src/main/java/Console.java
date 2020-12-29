47837dda8229ebd63a31e6437cc8c25186b1a481
ynfeng <fynwin@gmail.com>
2020-12-29 11:51:42 +0800

    optimize imports.

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
