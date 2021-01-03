a19206de774de55db6a65603a09b1feac0412388
ynfeng <fynwin@gmail.com>
2021-01-03 20:39:04 +0800

    refactor.

--- src/main/java/com/github/ynfeng/todo/command/MarkItemDoneCommand.java
+++ src/main/java/com/github/ynfeng/todo/command/MarkItemDoneCommand.java
@@ -4,8 +4,6 @@ import static com.github.ynfeng.todo.Console.println;
 
 import com.github.ynfeng.todo.ApplicationContext;
 import com.github.ynfeng.todo.Args;
-import com.github.ynfeng.todo.TodoApplicationException;
-import com.github.ynfeng.todo.todolist.Item;
 import com.github.ynfeng.todo.todolist.TodoList;
 import com.github.ynfeng.todo.user.CurrentUser;
 
@@ -14,10 +12,8 @@ public class MarkItemDoneCommand implements Command {
     public void execute(ApplicationContext context, Args args) {
         TodoList todoList = context.todoList(CurrentUser.username());
         int itemIdx = Integer.parseInt(args.getByIndex(1, "Usage: done <item index>"));
-        Item item = todoList.get(itemIdx - 1).orElseThrow(
-            () -> new TodoApplicationException("No such item"));
-        item.done();
-        todoList.update(item);
+        todoList.itemDone(itemIdx);
         println("Item %d done", itemIdx);
     }
+
 }
--- src/main/java/com/github/ynfeng/todo/todolist/TodoList.java
+++ src/main/java/com/github/ynfeng/todo/todolist/TodoList.java
@@ -1,5 +1,6 @@
 package com.github.ynfeng.todo.todolist;
 
+import com.github.ynfeng.todo.TodoApplicationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +14,11 @@ public interface TodoList {
     List<Item> all();
 
     void update(Item item);
+
+    default void itemDone(int itemIdx) {
+        Item item = get(itemIdx - 1).orElseThrow(
+            () -> new TodoApplicationException("No such item"));
+        item.done();
+        update(item);
+    }
 }
