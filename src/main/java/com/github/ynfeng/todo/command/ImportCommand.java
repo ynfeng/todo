6e3335f55fbd5977153f1c836d98aa24e00049b6
ynfeng <fynwin@gmail.com>
2021-01-04 19:59:40 +0800

    refactor code.

--- src/main/java/com/github/ynfeng/todo/command/ImportCommand.java
+++ src/main/java/com/github/ynfeng/todo/command/ImportCommand.java
@@ -5,14 +5,12 @@ import com.github.ynfeng.todo.Args;
 import com.github.ynfeng.todo.persistence.FileStorage;
 import com.github.ynfeng.todo.todolist.Item;
 import com.github.ynfeng.todo.user.CurrentUser;
-import java.util.List;
 
 public class ImportCommand implements Command {
     @Override
     public void execute(ApplicationContext context, Args args) {
         String importPath = args.getByIndex(2, "Usage: import -f <file path>");
         FileStorage<Item> importFile = new FileStorage<>(importPath, Item.class);
-        List<Item> itemList = importFile.loadAll();
-        context.todoList(CurrentUser.username()).addAll(itemList);
+        context.todoList(CurrentUser.username()).addAll(importFile.loadAll());
     }
 }
