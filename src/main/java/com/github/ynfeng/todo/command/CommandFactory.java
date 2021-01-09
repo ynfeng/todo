e57be2ae7cf9e2537921ea60d1ca302a823e2bbe
ynfeng <fynwin@gmail.com>
2021-01-09 11:55:17 +0800

    refactor.

--- src/main/java/com/github/ynfeng/todo/command/CommandFactory.java
+++ src/main/java/com/github/ynfeng/todo/command/CommandFactory.java
@@ -1,38 +1,35 @@
 package com.github.ynfeng.todo.command;
 
 import com.github.ynfeng.todo.TodoApplicationException;
+import com.google.common.collect.Maps;
+import java.util.Map;
 
 public final class CommandFactory {
+    private static final Map<String, Command> SUPPORT_COMMANDS = Maps.newHashMap();
+
+    static {
+        SUPPORT_COMMANDS.put("add", new AddItemCommand());
+        SUPPORT_COMMANDS.put("list", new ListItemsCommand());
+        SUPPORT_COMMANDS.put("done", new MarkItemDoneCommand());
+        SUPPORT_COMMANDS.put("login", new LoginCommand());
+        SUPPORT_COMMANDS.put("logout", new LogoutCommand());
+        SUPPORT_COMMANDS.put("adduser", new AddUserCommand());
+        SUPPORT_COMMANDS.put("export", new ExportCommand());
+        SUPPORT_COMMANDS.put("import", new ImportCommand());
+        SUPPORT_COMMANDS.put("dbconf", new DBConfigCommand());
+        SUPPORT_COMMANDS.put("init", new InitCommand());
+        SUPPORT_COMMANDS.put("whomi", new WhomiCommand());
+    }
+
     private CommandFactory() {
 
     }
 
     public static Command createCommand(String cmd) {
-        switch (cmd) {
-            case "add":
-                return new AddItemCommand();
-            case "list":
-                return new ListItemsCommand();
-            case "done":
-                return new MarkItemDoneCommand();
-            case "login":
-                return new LoginCommand();
-            case "logout":
-                return new LogoutCommand();
-            case "adduser":
-                return new AddUserCommand();
-            case "export":
-                return new ExportCommand();
-            case "import":
-                return new ImportCommand();
-            case "dbconf":
-                return new DBConfigCommand();
-            case "init":
-                return new InitCommand();
-            case "whomi":
-                return new WhomiCommand();
-            default:
-                throw new TodoApplicationException("unsupported command.");
+        Command command = SUPPORT_COMMANDS.get(cmd);
+        if (command == null) {
+            throw new TodoApplicationException("unsupported command.");
         }
+        return command;
     }
 }
