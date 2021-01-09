af31b40c3f24b0449963664167ed8b42a8fbb77c
ynfeng <fynwin@gmail.com>
2021-01-09 11:56:42 +0800

    refactor.

--- src/main/java/com/github/ynfeng/todo/command/CommandFactory.java
+++ src/main/java/com/github/ynfeng/todo/command/CommandFactory.java
@@ -5,20 +5,20 @@ import com.google.common.collect.Maps;
 import java.util.Map;
 
 public final class CommandFactory {
-    private static final Map<String, Command> SUPPORT_COMMANDS = Maps.newHashMap();
+    private static final Map<String, Command> SUPPORTED_COMMANDS = Maps.newHashMap();
 
     static {
-        SUPPORT_COMMANDS.put("add", new AddItemCommand());
-        SUPPORT_COMMANDS.put("list", new ListItemsCommand());
-        SUPPORT_COMMANDS.put("done", new MarkItemDoneCommand());
-        SUPPORT_COMMANDS.put("login", new LoginCommand());
-        SUPPORT_COMMANDS.put("logout", new LogoutCommand());
-        SUPPORT_COMMANDS.put("adduser", new AddUserCommand());
-        SUPPORT_COMMANDS.put("export", new ExportCommand());
-        SUPPORT_COMMANDS.put("import", new ImportCommand());
-        SUPPORT_COMMANDS.put("dbconf", new DBConfigCommand());
-        SUPPORT_COMMANDS.put("init", new InitCommand());
-        SUPPORT_COMMANDS.put("whomi", new WhomiCommand());
+        SUPPORTED_COMMANDS.put("add", new AddItemCommand());
+        SUPPORTED_COMMANDS.put("list", new ListItemsCommand());
+        SUPPORTED_COMMANDS.put("done", new MarkItemDoneCommand());
+        SUPPORTED_COMMANDS.put("login", new LoginCommand());
+        SUPPORTED_COMMANDS.put("logout", new LogoutCommand());
+        SUPPORTED_COMMANDS.put("adduser", new AddUserCommand());
+        SUPPORTED_COMMANDS.put("export", new ExportCommand());
+        SUPPORTED_COMMANDS.put("import", new ImportCommand());
+        SUPPORTED_COMMANDS.put("dbconf", new DBConfigCommand());
+        SUPPORTED_COMMANDS.put("init", new InitCommand());
+        SUPPORTED_COMMANDS.put("whomi", new WhomiCommand());
     }
 
     private CommandFactory() {
@@ -26,7 +26,7 @@ public final class CommandFactory {
     }
 
     public static Command getCommand(String cmd) {
-        Command command = SUPPORT_COMMANDS.get(cmd);
+        Command command = SUPPORTED_COMMANDS.get(cmd);
         if (command == null) {
             throw new TodoApplicationException("unsupported command.");
         }
